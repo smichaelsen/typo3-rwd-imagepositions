dbd27afb7c47ff8f5b2467cbf2db6d828ff5f36c
Sebastian Michaelsen <sebastian@michaelsen.io>
2015-04-02 11:42:10 +0200

    initial

---
+++ ext_emconf.php
@@ -0,0 +1,16 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'RWD Image Positions',
+	'description' => 'Allows you to position images in a responsive layout based on bootstrap',
+	'category' => 'plugin',
+	'author' => 'Sebastian Michaelsen',
+	'author_email' => 'sebastian@michaelsen.io',
+	'state' => 'beta',
+	'version' => '1.0.0',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '6.2.0-7.99.99',
+		),
+	),
+);
